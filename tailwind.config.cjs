0705c1c2d8764ddee3014df50dd796d3bbce533c
Xzayler <csak.mityu@gmail.com>
2024-03-21 14:58:34 +0100

    Add reaction icon colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -15,6 +15,9 @@ module.exports = {
         highlight: "rgb(var(--color-highlight) / <alpha-value>)",
         highlightextra: "rgb(var(--color-highlightextra) / <alpha-value>)",
         ui: "rgb(var(--color-divider) / <alpha-value>)",
+        like: "rgb(var(--color-like) / <alpha-value>)",
+        repost: "rgb(var(--color-repost) / <alpha-value>)",
+        comment: "rgb(var(--color-comment) / <alpha-value>)",
       },
       fontSize: {
         md: "15px",
