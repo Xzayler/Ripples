7c2905f73bbf354f110d361b18d0ef3ccd23ca75
Xzayler <csak.mityu@gmail.com>
2024-04-21 21:35:42 +0200

    cleanup: Removed console.logs

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,8 +1,6 @@
 export function calcDate(date: Date) {
   const now = new Date();
   const diff = Math.round((now.getTime() - date.getTime()) / 1000);
-  console.log(date);
-  console.log(diff);
   if (diff <= 60 * 60 * 24) {
     return `${Math.round(diff / 60)}m`;
   } else if (diff < 60 * 60 * 24) {
