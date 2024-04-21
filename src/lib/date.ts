66d92300c716995ad9a3217f13a4dc6838a541e5
Xzayler <csak.mityu@gmail.com>
2024-04-21 20:13:48 +0200

    fix: Relative post time calculation was off

--- src/lib/date.ts
+++ src/lib/date.ts
@@ -1,10 +1,12 @@
 export function calcDate(date: Date) {
   const now = new Date();
-  const diff = now.getTime() - date.getTime();
-  if (diff < 1000 * 60 * 60) {
-    return `${diff % (1000 * 60)}m`;
-  } else if (diff < 1000 * 60 * 60 * 24) {
-    return `${diff % (1000 * 60 * 60)}h`;
+  const diff = Math.round((now.getTime() - date.getTime()) / 1000);
+  console.log(date);
+  console.log(diff);
+  if (diff <= 60 * 60 * 24) {
+    return `${Math.round(diff / 60)}m`;
+  } else if (diff < 60 * 60 * 24) {
+    return `${Math.round(diff / (60 * 60))}h`;
   } else if (now.getFullYear() - date.getFullYear() == 0) {
     return `${date.toLocaleDateString("default", {
       month: "short",
