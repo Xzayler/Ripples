2918cdf4378e8757fae1c5cf60979f2ca084305c
Xzayler <csak.mityu@gmail.com>
2024-08-04 17:00:53 +0200

    fix: Add memo to keep bio reactive

--- src/components/shared/MultiLineText.tsx
+++ src/components/shared/MultiLineText.tsx
@@ -1,14 +1,17 @@
-import { For } from 'solid-js';
+import { createMemo, For } from 'solid-js';
 
 export default function MultiLineText(props: { text: string }) {
-  const lines = props.text.split('\n');
+  const lines = createMemo(() => {
+    return props.text.split('\n');
+  });
+
   return (
-    <For each={lines}>
+    <For each={lines()}>
       {(item, ind) => {
         return (
           <>
             {item}
-            {ind() < lines.length - 1 && <br />}
+            {ind() < lines().length - 1 && <br />}
           </>
         );
       }}
