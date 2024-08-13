63fca751689df195eef6f6e04c25e4b0f5d86361
Xzayler <csak.mityu@gmail.com>
2024-08-13 18:57:11 +0200

    fix: Display Loading component while loading

--- src/app.tsx
+++ src/app.tsx
@@ -2,23 +2,15 @@ import { Router } from '@solidjs/router';
 import { FileRoutes } from '@solidjs/start/router';
 import { Suspense } from 'solid-js';
 import './app.css';
-import WavesIcon from './components/shared/icons/WavesIcon';
 import { MetaProvider } from '@solidjs/meta';
+import Loading from './components/shared/Loading';
 
 export default function App() {
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <Suspense
-            fallback={
-              <div class="w-full h-dvh bg-background flex items-center justify-center  ">
-                <WavesIcon />
-              </div>
-            }
-          >
-            {props.children}
-          </Suspense>
+          <Suspense fallback={<Loading />}>{props.children}</Suspense>
         </MetaProvider>
       )}
     >
